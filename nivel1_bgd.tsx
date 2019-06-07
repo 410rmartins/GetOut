a9e2749d757481b8686f73512abff174c2a1f87c
410rmartins <45150070+410rmartins@users.noreply.github.com>
2019-06-07 12:36:31 +0100

    Add files via upload

---
+++ nivel1_bgd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="nivel1_bgd" tilewidth="32" tileheight="32" tilecount="9375" columns="125">
+ <image source="ProjetoMult/GetOut/assets/nivel1_bgd.png" width="4000" height="2418"/>
+</tileset>
